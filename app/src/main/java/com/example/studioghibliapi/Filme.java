7826bb0daaecbf6f64e728823e9ade29b550a265
ana-luizaa <ana.guglielmini@etec.sp.gov.br>
2021-03-24 23:56:27 -0300

    Tela sobre os filmes selecionados finalizada, assim como o resto da aplicação

--- app/src/main/java/com/example/studioghibliapi/Filme.java
+++ app/src/main/java/com/example/studioghibliapi/Filme.java
@@ -11,7 +11,7 @@ import android.widget.Toast;
 
 public class Filme extends AppCompatActivity {
 
-    TextView txtTitulo2, txtOriginal2, txtRomanizado2, txtDescricao2, txtDiretor2, txtAno2, txtTempo2, txtScore2;
+    TextView txtTitulo2, txtOriginal2, txtRomanizado2, txtDesc, txtDiretor2, txtAno2, txtTempo2, txtScore2;
     int id_To_Update = 0;
 
     Films films;
@@ -26,7 +26,7 @@ public class Filme extends AppCompatActivity {
         txtTitulo2 = findViewById(R.id.txtTitulo2);
         txtOriginal2 = findViewById(R.id.txtOriginal2);
         txtRomanizado2 = findViewById(R.id.txtRomanizado2);
-        txtDescricao2 = findViewById(R.id.txtDescricao2);
+        txtDesc = findViewById(R.id.txtDesc);
         txtDiretor2 = findViewById(R.id.txtDiretor2);
         txtAno2 = findViewById(R.id.txtAno2);
         txtTempo2 = findViewById(R.id.txtTempo2);
@@ -41,6 +41,8 @@ public class Filme extends AppCompatActivity {
                 Cursor rs  = db.getData(Value);
                 id_To_Update = Value;
                 if(rs.moveToFirst()){
+                    //Código original
+                    /*
                     films.setTitle(rs.getString(rs.getColumnIndex(db.FILMS_COLUMN_TITLE)));
                     films.setOriginal(rs.getString(rs.getColumnIndex(db.FILMS_COLUMN_ORIGINAL_TITLE)));
                     films.setTitle_romanised(rs.getString(rs.getColumnIndex(db.FILMS_COLUMN_ORIGINAL_TITLE_ROMANISED)));
@@ -48,7 +50,17 @@ public class Filme extends AppCompatActivity {
                     films.setDirector(rs.getString(rs.getColumnIndex(db.FILMS_COLUMN_DIRECTOR)));
                     films.setYear(rs.getString(rs.getColumnIndex(db.FILMS_COLUMN_RELEASE_DATE)));
                     films.setTime(rs.getString(rs.getColumnIndex(db.FILMS_COLUMN_RUNNING_TIME)));
-                    films.setScore(rs.getString(rs.getColumnIndex(db.FILMS_COLUMN_RT_SCORE)));
+                    films.setScore(rs.getString(rs.getColumnIndex(db.FILMS_COLUMN_RT_SCORE)));*/
+
+                    //código "limpo" segundo o Android Studio, apenas para não ter mais warnings de static member accessed via instance reference
+                    films.setTitle(rs.getString(rs.getColumnIndex(dbHelper.FILMS_COLUMN_TITLE)));
+                    films.setOriginal(rs.getString(rs.getColumnIndex(dbHelper.FILMS_COLUMN_ORIGINAL_TITLE)));
+                    films.setTitle_romanised(rs.getString(rs.getColumnIndex(dbHelper.FILMS_COLUMN_ORIGINAL_TITLE_ROMANISED)));
+                    films.setDescription(rs.getString(rs.getColumnIndex(dbHelper.FILMS_COLUMN_DESCRIPTION)));
+                    films.setDirector(rs.getString(rs.getColumnIndex(dbHelper.FILMS_COLUMN_DIRECTOR)));
+                    films.setYear(rs.getString(rs.getColumnIndex(dbHelper.FILMS_COLUMN_RELEASE_DATE)));
+                    films.setTime(rs.getString(rs.getColumnIndex(dbHelper.FILMS_COLUMN_RUNNING_TIME)));
+                    films.setScore(rs.getString(rs.getColumnIndex(dbHelper.FILMS_COLUMN_RT_SCORE)));
                 }
             } else{
                 Toast.makeText(Filme.this,"Ops! Ocorreu um erro.", Toast.LENGTH_SHORT).show();
@@ -56,7 +68,7 @@ public class Filme extends AppCompatActivity {
             txtTitulo2.setText(films.getTitle());
             txtOriginal2.setText(films.getOriginal());
             txtRomanizado2.setText(films.getTitle_romanised());
-            txtDescricao2.setText(films.getDescription());
+            txtDesc.setText(films.getDescription());
             txtDiretor2.setText(films.getDirector());
             txtAno2.setText(films.getYear());
             txtTempo2.setText(films.getTime());
