9eeb4a8b9b122d5457ecaa8fc27e324ed1d484af
ana-luizaa <ana.guglielmini@etec.sp.gov.br>
2021-06-13 16:07:27 -0300

    Colocando o sensor de luz e o gif

--- app/src/main/java/com/example/studioghibliapi/tela_inicio.java
+++ app/src/main/java/com/example/studioghibliapi/tela_inicio.java
@@ -2,11 +2,24 @@ package com.example.studioghibliapi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Service;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+//importando para sensor de luz
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.view.View;
+import android.widget.ImageView;
 
-public class tela_inicio extends AppCompatActivity {
+public class tela_inicio extends AppCompatActivity implements SensorEventListener{
+//definindo nomes
+    SensorManager sensorManager;
+    Sensor sensor;
+    ImageView totoro;
+    ImageView catbus;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +28,14 @@ public class tela_inicio extends AppCompatActivity {
 
         getSupportActionBar().hide();
 
+        //identificando os gifs
+        totoro = findViewById(R.id.totoro_andando);
+        catbus = findViewById(R.id.catbus);
+
+        //aplicando o sensor
+        sensorManager = (SensorManager) getSystemService(Service.SENSOR_SERVICE);
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
@@ -22,5 +43,35 @@ public class tela_inicio extends AppCompatActivity {
                 finish();
             }
         },5000);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener((SensorEventListener) this,sensor,SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_LIGHT) {
+            if (event.values[0] == 0) {
+                catbus.setVisibility(View.GONE);
+                totoro.setVisibility(View.VISIBLE);
+            } else {
+                catbus.setVisibility(View.VISIBLE);
+                totoro.setVisibility(View.GONE);
+            }
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 }
